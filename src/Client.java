9c79f3fee181db56360345c18bf7090963c5a862
lucccasm <lucas.montpetit@gmail.com>
2026-02-18 12:16:14 -0500

    ajout d'un boucle qui redemande le mdp et le nom de l'image tant que celui n'est pas valide

--- src/Client.java
+++ src/Client.java
@@ -19,41 +19,52 @@ public class Client {
         try (Scanner scanner = new Scanner(System.in)) {
             String serverAddress = NetworkUtils.askValidIp(scanner);
             int port = NetworkUtils.askValidPort(scanner);
+            boolean isAuthenticated = false;
 
-            System.out.print("Entrez un nom d'utilisateur: ");
-            userName = scanner.nextLine();
+            Socket socket = null;
+            DataOutputStream out = null;
+            DataInputStream in = null;
 
-            System.out.print("Entrez le mot de passe: ");
-            password = scanner.nextLine();
+            do {
+                System.out.print("Entrez un nom d'utilisateur: ");
+                userName = scanner.nextLine();
+                System.out.print("Entrez le mot de passe: ");
+                password = scanner.nextLine();
 
-            Socket socket = new Socket(serverAddress, port);
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                if (socket != null) socket.close();
+                socket = new Socket(serverAddress, port);
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
 
-            out.writeUTF(userName);
-            out.writeUTF(password);
-            out.flush();
+                out.writeUTF(userName);
+                out.writeUTF(password);
+                out.flush();
 
-            String authResponse = in.readUTF();
+                String authResponse = in.readUTF();
 
-            if(!authResponse.equals("OK")) {
-                System.out.println("Erreur dans la saisie du mot de passe");
-                socket.close();
-                return;
-            }
+                isAuthenticated = authResponse.equals("OK");
 
-            System.out.print("Entrez le nom de l'image à transformer (avec l'extension): ");
-            imageName = scanner.nextLine();
+                if (!isAuthenticated) {
+                    System.out.println("Erreur dans la saisie du mot de passe");
+                }
+            } while (!isAuthenticated);
+
+            File imageFile;
+
+            do {
+                System.out.print("Entrez le nom de l'image à transformer (avec l'extension): ");
+                imageName = scanner.nextLine();
+
+                imageFile = new File(imageName);
+
+                if (!imageFile.exists()) {
+                    System.out.println("Image introuvable.");
+                }
+            } while (!imageFile.exists());
 
             System.out.print("Entrez un nom pour l'image obtenue: ");
             imageNameAfterSobel = scanner.nextLine();
 
-            File imageFile = new File(imageName);
-            if (!imageFile.exists()) {
-                System.out.println("Image introuvable.");
-                socket.close();
-                return;
-            }
 
             // envoi de l'image
             byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
